0164e7470db4c32a0177a3e80bc4eb3af1d4da0b
Eftychios Sifakis <eftychios.sifakis.-nd@disneyresearch.com>
2019-09-30 12:09:37 -0500

    improved annotations and visual output for CG

--- Finite-Element-Tests/tests/MembranePinch6/ConjugateGradient.h
+++ Finite-Element-Tests/tests/MembranePinch6/ConjugateGradient.h
@@ -6,39 +6,40 @@ struct ConjugateGradient
         VectorType& q, VectorType& p, VectorType& r,
         const T tolerance,const int max_iterations)
     {
-        // NOTE: you should never try to make copies of VECTOR_T's inside here as they could be indirect.
-        T rhoOld=std::numeric_limits<T>::max();
-        T convergence_norm=0;
+        T rhoOld = std::numeric_limits<T>::max();
+        T convergence_norm = 0;
         r = b;
         system.Multiply(x, q);
         r -= q;
-        for(int iterations=0;;iterations++){
+
+        for(int iterations=0; ; iterations++){
 
             // check for convergence
             convergence_norm = system.Convergence_Norm(r);
-            std::cout << convergence_norm << std::endl;
+            std::cout << "Residual norm = " << convergence_norm << " [after " << iterations << " CG iterations]" << std::endl;
             if(convergence_norm <= tolerance) return true;
             if(iterations == max_iterations) break;
 
             // actual iteration
-            T rho = system.Inner_Product(r,r);
+            T rho = system.Inner_Product(r, r);
             if(iterations == 0)
                 p = r;
             else
-                p.Saxpy(rho/rhoOld,p,r);
+                p.Saxpy(rho / rhoOld, p, r); // rho/rhoOld is what is referred to as "beta"
+                                           // in the formal statement of the algorithm
             system.Multiply(p, q);
-            T p_dot_q = system.Inner_Product(p,q);
-            if(p_dot_q<=0)
+            T p_dot_q = system.Inner_Product(p, q);
+            if(p_dot_q <= 0)
                 std::cout << "CG: matrix appears indefinite or singular, p_dot_q/p_dot_p="
-                          << p_dot_q / (T)system.Inner_Product(p,p) << std::endl;
-            T alpha = rho/p_dot_q;
+                          << p_dot_q / system.Inner_Product(p, p) << std::endl;
+            T alpha = rho / p_dot_q;
             x.Saxpy(alpha, p, x);
             r.Saxpy(-alpha, q, r);
             rhoOld = rho;
             
         }
 
-        std::cout<<"cg not converged after "<<max_iterations<<" iterations, error = "<<convergence_norm<<std::endl;
+        std::cout << "cg not converged after " << max_iterations << " iterations, residual norm = " << convergence_norm<<std::endl;
         return false;
     }
 };
