dadc7387bbb3377a448d85e3ba6b77c8c4c9cf76
Eftychios Sifakis <sifakis@github.com>
2019-10-11 12:39:12 -0500

    adjusting CMakeLists.txt files

---
+++ Finite-Element-Tests/tests/MembraneStretch3/FiniteElementMesh.h
@@ -0,0 +1,186 @@
+#pragma once
+
+#include "AnimatedMesh.h"
+
+#include "CGVectorWrapper.h"
+#include "CGSystemWrapper.h"
+#include "ConjugateGradient.h"
+
+#include <Eigen/Dense>
+
+//#define USE_LINEAR_ELASTICITY
+#define USE_ST_VENANT_KIRCHHOFF
+
+template<class T>
+struct FiniteElementMesh : public AnimatedMesh<T, 3>
+{
+    using Base = AnimatedMesh<T, 3>;
+    using Base::m_meshElements;
+    using Base::m_particleX;
+    using Vector2 = typename Base::Vector2;
+    using Matrix22 = Eigen::Matrix< T , 2 , 2>;
+
+    int m_nFrames;
+    int m_subSteps;
+    T m_frameDt;
+    T m_stepDt;
+    T m_stepEndTime;
+
+    const T m_density;
+    const T m_mu;
+    const T m_lambda;
+    const T m_rayleighCoefficient;
+
+    std::vector<T> m_particleMass;
+    std::vector<Matrix22> m_DmInverse;
+    std::vector<T> m_restVolume;
+    
+    FiniteElementMesh(const T density, const T mu, const T lambda, const T rayleighCoefficient)
+        :m_density(density), m_mu(mu), m_lambda(lambda), m_rayleighCoefficient(rayleighCoefficient)
+    {}
+
+    void initializeUndeformedConfiguration()
+    {
+        // Initialize rest shape and particle mass (based on constant density)
+        m_particleMass.resize(m_particleX.size(), T()); // Initialize all particle masses to zero
+        for(const auto& element: m_meshElements)
+        {
+            Matrix22 Dm;
+            for(int j = 0; j < 2; j++)
+                Dm.col(j) = m_particleX[element[j+1]]-m_particleX[element[0]];
+            T restVolume = .5 * Dm.determinant();
+            if(restVolume < 0)
+                throw std::logic_error("Inverted element");
+            m_DmInverse.emplace_back(Dm.inverse());
+            m_restVolume.push_back(restVolume);
+            T elementMass = m_density * restVolume;
+            for(const int v: element)
+                m_particleMass[v] += (1./3.) * elementMass;
+        }
+    }
+    
+    void addElasticForce(std::vector<Vector2>& f) const
+    {
+        for(int e = 0; e < m_meshElements.size(); e++)
+        {
+            const auto& element = m_meshElements[e];
+
+            // Compute deformation gradient
+            Matrix22 Ds;
+            for(int j = 0; j < 2; j++)
+                Ds.col(j) = m_particleX[element[j+1]]-m_particleX[element[0]];
+            Matrix22 F = Ds * m_DmInverse[e];
+
+#ifdef USE_LINEAR_ELASTICITY
+            Matrix22 strain = .5 * (F + F.transpose()) - Matrix22::Identity();
+            Matrix22 P = 2. * m_mu * strain + m_lambda * strain.trace() * Matrix22::Identity();
+#endif
+
+#ifdef USE_ST_VENANT_KIRCHHOFF
+            Matrix22 E = .5 * ( F.transpose() * F - Matrix22::Identity());
+            Matrix22 P = F * (2. * m_mu * E + m_lambda * E.trace() * Matrix22::Identity());
+#endif
+            
+            Matrix22 H = -m_restVolume[e] * P * m_DmInverse[e].transpose();
+            
+            for(int j = 0; j < 2; j++){
+                f[element[j+1]] += H.col(j);
+                f[element[0]] -= H.col(j);
+            }
+        }
+    }
+
+    void addProductWithStiffnessMatrix(std::vector<Vector2>& dx, std::vector<Vector2>& df, const T scale) const
+    {
+        for(int e = 0; e < m_meshElements.size(); e++)
+        {
+            const auto& element = m_meshElements[e];
+
+            // Compute deformation gradient
+            Matrix22 Ds;
+            for(int j = 0; j < 2; j++)
+                Ds.col(j) = m_particleX[element[j+1]]-m_particleX[element[0]];
+            Matrix22 F = Ds * m_DmInverse[e];
+
+            // Compute differential(s)
+            Matrix22 dDs;
+            for(int j = 0; j < 2; j++)
+                dDs.col(j) = dx[element[j+1]]-dx[element[0]];
+            Matrix22 dF = dDs * m_DmInverse[e];
+
+#ifdef USE_LINEAR_ELASTICITY
+            Matrix22 dstrain = .5 * (dF + dF.transpose());
+            Matrix22 dP = scale * (2. * m_mu * dstrain + m_lambda * dstrain.trace() * Matrix22::Identity());
+#endif
+
+#ifdef USE_ST_VENANT_KIRCHHOFF
+            Matrix22 E = .5 * ( F.transpose() * F - Matrix22::Identity());
+            Matrix22 dE = .5 * ( dF.transpose() * F + F.transpose() * dF);
+            Matrix22 dP = dF * (2. * m_mu *  E + m_lambda *  E.trace() * Matrix22::Identity()) +
+                           F * (2. * m_mu * dE + m_lambda * dE.trace() * Matrix22::Identity());
+
+#endif
+            
+            Matrix22 dH = m_restVolume[e] * dP * m_DmInverse[e].transpose();
+            
+            for(int j = 0; j < 2; j++){
+                df[element[j+1]] += dH.col(j);
+                df[element[0]] -= dH.col(j);
+            }
+        }
+    }
+
+    void simulateSubstep()
+    {
+        using FEMType = FiniteElementMesh<T>;        
+
+        const int nParticles = m_particleX.size();
+
+        // Construct initial guess for next-timestep
+        //   Positions -> Same as last timestep
+        
+        // Overwrite boundary conditions with desired values
+
+        setBoundaryConditions();
+        
+        // Solve for everything else using Conjugate Gradients
+
+        std::vector<Vector2> dx(nParticles, Vector2::Zero());
+        std::vector<Vector2> rhs(nParticles, Vector2::Zero());
+        std::vector<Vector2> q(nParticles, Vector2::Zero());
+        std::vector<Vector2> s(nParticles, Vector2::Zero());
+        std::vector<Vector2> r(nParticles, Vector2::Zero());
+        CGVectorWrapper<Vector2> dxWrapper(dx);
+        CGVectorWrapper<Vector2> rhsWrapper(rhs);
+        CGVectorWrapper<Vector2> qWrapper(q);
+        CGVectorWrapper<Vector2> sWrapper(s);
+        CGVectorWrapper<Vector2> rWrapper(r);
+        CGSystemWrapper<Vector2, FEMType> systemWrapper(*this);
+        
+        addElasticForce(rhs);
+        clearConstrainedParticles(rhs);
+
+        ConjugateGradient<T>::Solve(systemWrapper,
+            dxWrapper, rhsWrapper, qWrapper, sWrapper, rWrapper,
+            1e-4, 50);
+
+        // Apply corrections to positions and velocities
+
+        for(int p = 0; p < nParticles; p++)
+            m_particleX[p] += dx[p];
+    }
+
+    void simulateFrame(const int frame)
+    {
+        m_stepDt = m_frameDt / (T) m_subSteps;
+
+        for(int step = 1; step <= m_subSteps; step++){
+            m_stepEndTime = m_frameDt * (T) (frame-1) + m_stepDt * (T) step;
+            simulateSubstep();
+        }
+    }
+
+    virtual void clearConstrainedParticles(std::vector<Vector2>& x) {}
+    virtual void setBoundaryConditions() {}
+};
+
---
+++ Finite-Element-Tests/tests/MembraneStretch3/main.cpp
@@ -0,0 +1,145 @@
+#include "FiniteElementMesh.h"
+
+#include <Eigen/Dense>
+
+template<class T>
+struct LatticeMesh : public FiniteElementMesh<T>
+{
+    using Base = FiniteElementMesh<T>;
+
+    // from AnimatedMesh
+    using Base::m_meshElements;
+    using Base::m_particleX;
+    using Base::initializeUSD;
+    using Base::initializeTopology;
+    using Base::initializeParticles;
+    using Vector2 = typename Base::Vector2;
+
+    // from FiniteElementMesh
+    using Base::initializeUndeformedConfiguration;
+    using Base::m_stepEndTime;
+
+    std::array<int, 2> m_cellSize; // dimensions in grid cells
+    T m_gridDX;
+
+    const int m_pinchRadius;
+
+    std::vector<Vector2> m_particleUndeformedX;
+    std::vector<int> m_leftHandleIndices;
+    std::vector<int> m_rightHandleIndices;
+    Vector2 m_leftHandleDisplacement;
+    Vector2 m_rightHandleDisplacement;
+    
+    LatticeMesh()
+        :Base(1.e2, 1., 4., .05), m_pinchRadius(1)
+    {
+        m_leftHandleDisplacement  = Vector2(-.5, 0.);
+        m_rightHandleDisplacement = Vector2(-.5, 0.);
+    }
+
+    void initialize()
+    {
+        initializeUSD("MembraneStretch3.usda");
+
+        // Create a Cartesian lattice topology
+        for(int cell_i = 0; cell_i < m_cellSize[0]; cell_i++)
+        for(int cell_j = 0; cell_j < m_cellSize[1]; cell_j++){
+            m_meshElements.emplace_back(
+                std::array<int, 3>{
+                    gridToParticleID(cell_i  , cell_j  ), 
+                    gridToParticleID(cell_i+1, cell_j  ),
+                    gridToParticleID(cell_i+1, cell_j+1)
+                }
+            );
+            m_meshElements.emplace_back(
+                std::array<int, 3>{
+                    gridToParticleID(cell_i  , cell_j  ), 
+                    gridToParticleID(cell_i+1, cell_j+1),
+                    gridToParticleID(cell_i  , cell_j+1)
+                }
+            );
+        }
+        initializeTopology();
+
+        // Also initialize the associated particles
+        for(int node_i = 0; node_i <= m_cellSize[0]; node_i++)
+        for(int node_j = 0; node_j <= m_cellSize[1]; node_j++)
+            m_particleX.emplace_back(m_gridDX * (T)node_i, m_gridDX * (T)node_j);
+        initializeParticles();
+
+        // Check particle indexing in mesh
+        for(const auto& element: m_meshElements)
+            for(const auto vertex: element)
+                if(vertex < 0 || vertex >= m_particleX.size())
+                    throw std::logic_error("mismatch between mesh vertex and particle array");
+
+        // Initialize rest shape matrices and particle mass
+        initializeUndeformedConfiguration();
+
+        // Also record rest shape
+        m_particleUndeformedX = m_particleX;
+
+        // Identify particles on left and right handles
+        for(int node_j = 0; node_j <= m_cellSize[1]; node_j++){
+            m_leftHandleIndices.push_back(gridToParticleID(0, node_j));
+            m_rightHandleIndices.push_back(gridToParticleID(m_cellSize[0], node_j));
+        }
+    }
+
+    void initializeDeformation()
+    {
+        // No need to apply any deformation; this example is driven by moving handles
+    }
+
+    void clearConstrainedParticles(std::vector<Vector2>& x) override
+    { 
+        for(const auto v: m_leftHandleIndices)
+            x[v] = Vector2::Zero();
+        for(const auto v: m_rightHandleIndices)
+            x[v] = Vector2::Zero();       
+    }
+
+    void setBoundaryConditions() override
+    { 
+        T effectiveTime = std::min<T>(m_stepEndTime, 1.0);
+        
+        for(const auto v: m_leftHandleIndices){
+            m_particleX[v] = m_particleUndeformedX[v] + effectiveTime * m_leftHandleDisplacement;
+        }
+        for(const auto v: m_rightHandleIndices){
+            m_particleX[v] = m_particleUndeformedX[v] + effectiveTime * m_rightHandleDisplacement;
+        }
+    }
+
+private:
+    inline int gridToParticleID(const int i, const int j) const { return i * (m_cellSize[1]+1) + j; }
+};
+
+int main(int argc, char *argv[])
+{
+    LatticeMesh<float> simulationMesh;
+    simulationMesh.m_cellSize = { 40, 40 };
+    simulationMesh.m_gridDX = 0.05;
+    simulationMesh.m_nFrames = 10;
+    simulationMesh.m_subSteps = 1;
+    simulationMesh.m_frameDt = 1.0;
+
+    // Initialize the simulation example
+    simulationMesh.initialize();
+    simulationMesh.initializeDeformation();
+
+    // Output the initial shape of the mesh
+    simulationMesh.writeFrame(0);
+
+    // Perform the animation, output results at each frame
+    for(int frame = 1; frame <= simulationMesh.m_nFrames; frame++){
+        simulationMesh.simulateFrame(frame);
+        simulationMesh.writeFrame(frame);
+    }
+
+    // Write the entire timeline to USD
+    simulationMesh.writeUSD();
+
+    return 0;
+}
+
